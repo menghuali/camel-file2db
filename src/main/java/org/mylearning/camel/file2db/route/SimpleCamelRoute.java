fe8edc7dac2bdc0be97e094d1ed9434ef96dae31
mli <menghua.li@westjet.com>
2020-12-28 20:21:44 -0500

    used lombok annotation

--- src/main/java/org/mylearning/camel/file2db/route/SimpleCamelRoute.java
+++ src/main/java/org/mylearning/camel/file2db/route/SimpleCamelRoute.java
@@ -3,11 +3,15 @@ package org.mylearning.camel.file2db.route;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Component
+@Slf4j
 public class SimpleCamelRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+        log.info("Inside of configure method");
         from("timer:hello?period={{poll_interval}}").log("Time invoked")
                 .pollEnrich("file:{{input_folder}}?delete=true&readLock=none").log("Body is: ${body}")
                 .to("file:{{output_folder}}");
