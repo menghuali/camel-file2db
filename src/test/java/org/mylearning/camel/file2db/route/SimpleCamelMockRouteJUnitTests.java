704ae953114c97d8824b4d835459987820d78f78
mli <menghua.li@westjet.com>
2020-12-29 20:20:50 -0500

    use H2 to unit test jdbc endpoint

--- src/test/java/org/mylearning/camel/file2db/route/SimpleCamelMockRouteJUnitTests.java
+++ src/test/java/org/mylearning/camel/file2db/route/SimpleCamelMockRouteJUnitTests.java
@@ -1,18 +1,31 @@
 package org.mylearning.camel.file2db.route;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Properties;
 
+import javax.sql.DataSource;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.RoutesBuilder;
+import org.apache.camel.spi.Registry;
 import org.apache.camel.test.junit5.CamelTestSupport;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.util.FileSystemUtils;
 
 public class SimpleCamelMockRouteJUnitTests extends CamelTestSupport {
     private static Properties props;
+    private static final String DUMMY_QUERY = "SELECT 'Hello world'";
 
     @BeforeAll
     public static void beforeAll() {
@@ -33,13 +46,25 @@ public class SimpleCamelMockRouteJUnitTests extends CamelTestSupport {
         getMockEndpoint(String.format("mock:file:%s?delete=true&readLock=none", props.getProperty("input_folder")))
                 .expectedBodiesReceived(body);
         getMockEndpoint(String.format("mock:file:%s", props.getProperty("output_folder"))).expectedBodiesReceived(body);
+        getMockEndpoint("mock:jdbc:dataSource").expectedBodiesReceived(DUMMY_QUERY);
         template.sendBody(String.format("file:%s?delete=true&readLock=none", props.getProperty("input_folder")), body);
         assertMockEndpointsSatisfied();
     }
 
     @Override
     protected RoutesBuilder createRouteBuilder() throws Exception {
-        return new SimpleCamelRoute();
+        SimpleCamelRoute route = new SimpleCamelRoute();
+        Processor buildSQLProcessor = mock(Processor.class);
+        route.setBuildSQLProcessor(buildSQLProcessor);
+        doAnswer(new Answer<Object>() {
+            @Override
+            public Object answer(InvocationOnMock arg) throws Throwable {
+                Exchange exchange = arg.getArgument(0);
+                exchange.getIn().setBody(DUMMY_QUERY);
+                return null;
+            }
+        }).when(buildSQLProcessor).process(any(Exchange.class));
+        return route;
     }
 
     @Override
@@ -50,6 +75,13 @@ public class SimpleCamelMockRouteJUnitTests extends CamelTestSupport {
     @Override
     protected Properties useOverridePropertiesWithPropertiesComponent() {
         return props;
+    }
+
+    @Override
+    protected void bindToRegistry(Registry registry) throws Exception {
+        DataSource ds = new DriverManagerDataSource("jdbc:h2:mem:testdb");
+        registry.bind("dataSource", ds);
+        super.bindToRegistry(registry);
     }
 
 }
